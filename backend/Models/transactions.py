7d23329f39b037e75cbca1170619fd80fa028ece
Marvin <mdtensuan_dev@protonmail.com>
2022-01-15 16:41:36 +0800

    🐍 Set-up of DB config.

--- backend/Models/transactions.py
+++ backend/Models/transactions.py
@@ -5,22 +5,25 @@ from enum import Enum
 from pydantic import condecimal
 from sqlmodel import SQLModel, Field
 
-class Transactions(SQLModel):
-    date: date
-    account: str
-    amount: condecimal(decimal_places=2) = Field(default=0)
-    category: TransactionCategory | None
-    description: str
 
 class TransactionCategory(str, Enum):
-    PERSONAL_EXPENSE = 'Personal Expense'
-    MAJOR_EXPENSE = 'Major Expense'
-    FINANCE_CHARGE = 'Finance Charge'
-    INTEREST_EXPENSE = 'Interest Expense'
-    INTEREST_REVENUE = 'Interest Revenue'
-    WTH_TAX = 'Withholding Taxes'
-    BILLS = 'Bills'
-    CASH_TRANSFER = 'Cash Transfer'
-    INSURANCE = 'Insurance'
-    INVESTMENT_DEPOSIT = 'Investment Deposit'
-    PAYROLL = 'Payroll'
+    PERSONAL_EXPENSE = "Personal Expense"
+    MAJOR_EXPENSE = "Major Expense"
+    FINANCE_CHARGE = "Finance Charge"
+    INTEREST_EXPENSE = "Interest Expense"
+    INTEREST_REVENUE = "Interest Revenue"
+    WTH_TAX = "Withholding Taxes"
+    BILLS = "Bills"
+    CASH_TRANSFER = "Cash Transfer"
+    INSURANCE = "Insurance"
+    INVESTMENT_DEPOSIT = "Investment Deposit"
+    PAYROLL = "Payroll"
+
+
+class Transactions(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    date: date
+    account: str
+    amount: float
+    category: TransactionCategory | None = None
+    description: str
--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,21 @@
+import tomli
+
 from fastapi import FastAPI
+from sqlmodel import Session, create_engine
+
+from Models.transactions import Transactions, TransactionCategory
+
+with open("config.toml") as file:
+    config = tomli.loads(file.read())
+    db = config.get("db")
+
+engine = create_engine(db.get("url"))
 
 app = FastAPI()
 
+
 @app.get("/tables/{table_name}")
 async def read_from_table(table_name):
+    with Session(engine) as session:
+        ...
     return 200
