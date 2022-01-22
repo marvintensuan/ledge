065df054c627aeaca74574d752467793a6cc0afd
Marvin <mdtensuan_dev@protonmail.com>
2022-01-22 19:23:54 +0800

    🎨 Fetch data from backend API.

--- ledge-front/pages/transactions.vue
+++ ledge-front/pages/transactions.vue
@@ -1,7 +1,12 @@
 <script>
 export default {
+  data(){ return{ transactions: [] } },
+  async fetch(){
+      this.transactions = await fetch("http://127.0.0.1:8000/tables/transactions")
+        .then(response => response.json())
+  },
   computed: {
-    transactions() { return this.$store.state.tables.tables.transactions}
+    transactions() { return this.$store.state.tables.transactions} 
   }
 }
 </script>
@@ -10,10 +15,22 @@ export default {
   <main>
     <h1>Transactions</h1>
     <table>
-      <thead>{{ transactions.Head }}</thead>
-      <tbody>
+      <thead>
         <tr>
-          <td>{{ transactions.Body }}</td>
+          <th>Date</th>
+          <th>Account</th>
+          <th>Amount (PHP)</th>
+          <th>Category</th>
+          <th>Description</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="transaction in transactions" :key="transaction.id">
+          <td>{{ transaction.date }}</td>
+          <td>{{ transaction.account }}</td>
+          <td>{{ transaction.amount }}</td>
+          <td>{{ transaction.category }}</td>
+          <td>{{ transaction.description }}</td>
         </tr>
       </tbody>
     </table>
