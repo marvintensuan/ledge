7d955f1ac3a82ab0020ceffc7fdfebcc609200ec
Marvin <mdtensuan_dev@protonmail.com>
2022-02-06 15:14:32 +0800

    Created custom component for Transactions table.

---
+++ ledge-front/components/TransactionsTable.vue
@@ -0,0 +1,45 @@
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th v-for="item in tableHeaders" :key="item" class="capitalize">
+          {{ item }}
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="transaction in tableData" :key="transaction.id">
+        <td>{{ transaction.date }}</td>
+        <td>{{ transaction.account }}</td>
+        <td>
+          {{
+            transaction.amount.toLocaleString('en-US', {
+              minimumFractionDigits: 2,
+            })
+          }}
+        </td>
+        <td>{{ transaction.category }}</td>
+        <td>{{ transaction.description }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  props: {
+    tableData: Array,
+  },
+  data() {
+    return {
+      tableHeaders: [
+        'Date',
+        'Account',
+        'Amount (PHP)',
+        'Category',
+        'Description',
+      ],
+    }
+  },
+}
+</script>
--- ledge-front/pages/transactions.vue
+++ ledge-front/pages/transactions.vue
@@ -1,44 +1,26 @@
 <script>
+import Table from '~/components/TransactionsTable.vue'
 export default {
   async fetch() {
-    let isEmpty = this.$store.state.tables.transactions;
+    let isEmpty = this.$store.state.tables.transactions
     if (!isEmpty.length) {
-      let results = await this.$axios.get("http://127.0.0.1:8000/tables/transactions");
-      this.$store.commit("tables/addTransaction", results.data);
+      let results = await this.$axios.get(
+        'http://127.0.0.1:8000/tables/transactions'
+      )
+      this.$store.commit('tables/addTransaction', results.data)
     }
   },
   computed: {
     transactions() {
-      return this.$store.state.tables.transactions;
+      return this.$store.state.tables.transactions
     },
   },
-};
+  components: { Table },
+}
 </script>
 
 <template>
   <main>
-    <h1>Transactions</h1>
-    <table>
-      <thead>
-        <tr>
-          <th>Date</th>
-          <th>Account</th>
-          <th>Amount (PHP)</th>
-          <th>Category</th>
-          <th>Description</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="transaction in transactions" :key="transaction.id">
-          <td>{{ transaction.date }}</td>
-          <td>{{ transaction.account }}</td>
-          <td>
-            {{ transaction.amount.toLocaleString("en-US", { minimumFractionDigits: 2 }) }}
-          </td>
-          <td>{{ transaction.category }}</td>
-          <td>{{ transaction.description }}</td>
-        </tr>
-      </tbody>
-    </table>
+    <TransactionsTable :tableData="transactions" />
   </main>
 </template>
