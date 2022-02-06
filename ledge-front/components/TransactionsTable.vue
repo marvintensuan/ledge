0b14ae34367d1de42fdb09dc40729d24ebeea581
Marvin <mdtensuan_dev@protonmail.com>
2022-02-06 17:12:02 +0800

    Added form.

--- ledge-front/components/TransactionsTable.vue
+++ ledge-front/components/TransactionsTable.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <table class="m-auto divide-x divide-y">
+  <div class="flex flex-col m-auto">
+    <table class="divide-x divide-y">
       <thead class="bg-gray-50">
         <tr>
           <th scope="col" v-for="item in tableHeaders" :key="item">
@@ -14,7 +14,7 @@
           :key="transaction.id"
           class="divide-y divide-gray-200"
         >
-          <td class="w-36">{{ transaction.date }}</td>
+          <td>{{ transaction.date }}</td>
           <td class="w-36">{{ transaction.account }}</td>
           <td class="w-32 text-right">
             {{
@@ -26,8 +26,36 @@
           <td class="w-36">{{ transaction.category }}</td>
           <td class="w-36">{{ transaction.description }}</td>
         </tr>
+        <tr class="bg-gray-300">
+          <td><input type="date" form="newEntry" /></td>
+          <td>
+            <select form="newEntry" class="w-36">
+              <option>FOO</option>
+              <option>Bar</option>
+              <option>bAz</option>
+            </select>
+          </td>
+          <td>
+            <input
+              type="number"
+              form="newEntry"
+              value="0.00"
+              class="text-right"
+            />
+          </td>
+          <td><input type="text" form="newEntry" value="..." /></td>
+          <td><input type="text" form="newEntry" value="..." /></td>
+        </tr>
       </tbody>
     </table>
+    <form id="newEntry" class="m-auto mt-5">
+      <button
+        type="button"
+        onclick="javascript:console.log(document.getElementById('newEntry').data)"
+      >
+        Submit
+      </button>
+    </form>
   </div>
 </template>
 
